6f11737b1473cbb39c70a9ab8829924b201b5686
Daniela Pinto Veizaga <danielapintoveizaga@gmail.com>
2021-11-18 03:59:08 -0400

    release 0.11

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ author='Daniela Pinto Veizaga',
 author_email='danielapintoveizaga@gmail.com',
 description='A python wrapper for users of the Academic Research product track',
 packages=['tweetple'],
+url='https://github.com/dapivei/tweetple'
 zip_safe=False
 )
